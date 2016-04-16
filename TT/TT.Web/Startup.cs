0325fe2c3ee9576ec1b20f030e7bc46fb5bf671b
Dmitry Cherepanov <chrd@bk.ru>
2016-04-16 09:16:47 +0600

    first commit

---
+++ TT/TT.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TT.Web.Startup))]
+namespace TT.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
