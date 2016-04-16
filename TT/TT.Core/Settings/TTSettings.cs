575eaf734e59243d408a358f0b2b5a6c1800a2ee
Alexander Biryukov <aleksbiryukov@gmail.com>
2016-04-16 13:44:12 +0600

    moar WS stuff

--- TT/TT.Core/Settings/TTSettings.cs
+++ TT/TT.Core/Settings/TTSettings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Runtime.CompilerServices;
 
 namespace TT.Core.Settings
@@ -24,6 +25,52 @@ namespace TT.Core.Settings
         private static bool GetBoolean(string settingName)
         {
             return Convert.ToBoolean(SharedConfiguration.Instance.AppSettings.Settings[settingName].Value);
+        }
+
+        /// <summary>
+        /// Gets a value from the AppSettings config section, casted to type T
+        /// </summary>
+        /// <typeparam name="T">Type to cast value to</typeparam>
+        /// <param name="key">AppSetting key</param>
+        /// <param name="defaultVal">Default value on error</param>
+        /// <param name="throwOnException">Whether to throw an exception on error</param>
+        /// <param name="warnOnMissingFromConfig">Whether to log warn if missing from config. This should ONLY be set
+        /// to false if you don't plan on putting the setting in the config, but want to allow adding the setting to
+        /// config if need be. *This parameter is not used if throwOnException is true.</param>
+        /// <returns></returns>
+        public static T GetAppSetting<T>(string key, T defaultVal = default(T), bool throwOnException = false, bool warnOnMissingFromConfig = true)
+        {
+            T val;
+            try
+            {
+                var reader = new AppSettingsReader();
+                val = (T)reader.GetValue(key, typeof(T));
+            }
+            catch (ArgumentNullException) // Key or T is null
+            {
+                if (throwOnException)
+                    throw;
+                if (warnOnMissingFromConfig)
+                    Logger.Logger.Current.Warning($"App setting '{key}' is null; using default: {defaultVal}");
+                val = defaultVal;
+            }
+            catch (InvalidOperationException ex) // Value could not be parsed to type T
+            {
+                if (throwOnException)
+                    throw;
+                if (ex.Message.Contains("does not exist in the appSettings configuration section"))
+                {
+                    if (warnOnMissingFromConfig)
+                        Logger.Logger.Current.Warning(
+                            $"App setting '{key}' does not exist in the appSettings configuration section; using default: {defaultVal}");
+                }
+                else
+                {
+                    Logger.Logger.Current.Error($"Using default for AppSetting '{key}': {defaultVal}; {ex.Message}");
+                }
+                val = defaultVal;
+            }
+            return val;
         }
     }
 }
