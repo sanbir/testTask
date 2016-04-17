fd60aa0964b34c2e24e1b6f9a4fcf0e9e41bc28a
Dmitry Cherepanov <chrd@bk.ru>
2016-04-17 15:18:01 +0600

    web

--- TT/TT.Web/Controllers/ChartController.cs
+++ TT/TT.Web/Controllers/ChartController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TT.DAL.Repository;
 
 namespace TT.Web.Controllers
 {
     public class ChartController : Controller
     {
-        // GET: Chart
+        private IQuoteRepository _quoteRepository;
+
+        public ChartController(IQuoteRepository quoteRepository)
+        {
+            _quoteRepository = quoteRepository;
+        }
+
+        public ChartController(): this(new QuoteRepository())
+        {
+            
+        }
+
         public ActionResult Index()
         {
             return View();
         }
+
+      /*  public JsonResult GetCurrencyList()
+        {
+            _quoteRepository.Get()
+        }
+*/
     }
 }
